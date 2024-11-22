c9caef7d534960df2cbe547511c2b4638bad4b00
Joel Gustafson <joelg@mit.edu>
2024-11-22 14:19:14 -0500

    add eql and expectEqual

--- src/multihash/lib.zig
+++ src/multihash/lib.zig
@@ -40,22 +40,39 @@ pub const Digest = struct {
         allocator.free(self.hash);
     }
 
+    pub fn copy(self: Digest, allocator: std.mem.Allocator) !Digest {
+        const hash = try allocator.alloc(u8, self.hash.len);
+        @memcpy(hash, self.hash);
+        return .{ .code = self.code, .hash = hash };
+    }
+
     pub fn eql(self: Digest, other: Digest) bool {
         return self.code == other.code and std.mem.eql(u8, self.hash, other.hash);
     }
 
+    pub fn expectEqual(actual: Digest, expected: Digest) !void {
+        try std.testing.expectEqual(actual.code, expected.code);
+        try std.testing.expectEqualSlices(u8, actual.hash, expected.hash);
+    }
+
+    pub fn encodingLength(self: Digest) usize {
+        const code_len = self.code.encodingLength();
+        const size_len = varint.encodingLength(self.hash.len);
+        return code_len + size_len + self.hash.len;
+    }
+
     pub fn encode(self: Digest, allocator: std.mem.Allocator) ![]const u8 {
-        const code_len = Codec.encodingLength(self.code);
+        const code_len = self.code.encodingLength();
         const size_len = varint.encodingLength(self.hash.len);
         const bytes = try allocator.alloc(u8, code_len + size_len + self.hash.len);
-        _ = Codec.encode(bytes, self.code);
+        _ = self.code.encode(bytes);
         _ = varint.encode(bytes[code_len..], self.hash.len);
         @memcpy(bytes[code_len + size_len ..], self.hash);
         return bytes;
     }
 
     pub fn write(self: Digest, writer: std.io.AnyWriter) !void {
-        try Codec.write(writer, self.code);
+        try self.code.write(writer);
         try varint.write(writer, self.hash.len);
         try writer.writeAll(self.hash);
     }
